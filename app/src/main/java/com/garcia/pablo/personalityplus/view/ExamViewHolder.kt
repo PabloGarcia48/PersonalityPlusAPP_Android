54de82137fc375e132ed6a701f758b49d6f00e26
PabloGarcia48 <pabloroberto2011@gmail.com>
2022-10-06 20:03:19 -0300

    Added all of descriptions inside of options, and created radiobutton

--- app/src/main/java/com/garcia/pablo/personalityplus/view/ExamViewHolder.kt
+++ app/src/main/java/com/garcia/pablo/personalityplus/view/ExamViewHolder.kt
@@ -2,11 +2,19 @@ package com.garcia.pablo.personalityplus.view
 
 import androidx.recyclerview.widget.RecyclerView
 import com.garcia.pablo.personalityplus.databinding.ItemStepBinding
+import com.garcia.pablo.personalityplus.model.entities.Option
 import com.garcia.pablo.personalityplus.model.entities.Step
 
 class ExamViewHolder(val binding: ItemStepBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: Step) {
-        binding.tvDescription.text = item.options[0].description
+        binding.rbOption0.text = getOptionText(item.options[0])
+        binding.rbOption1.text = getOptionText(item.options[1])
+        binding.rbOption2.text = getOptionText(item.options[2])
+        binding.rbOption3.text = getOptionText(item.options[3])
+    }
+
+    fun getOptionText(option: Option): String {
+        return "${option.featureName}: ${option.description}"
     }
 }
